21635ce8ce87f3b9d2ddd730abad774061ff796e
Pablokill2004 <109759348+Pablokill2004@users.noreply.github.com>
2024-04-26 17:43:44 +0000

    Lab10 Completed!

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,38 @@ import './App.css';
 function App() {
   const [allTodos, setAllTodos] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [countTask, setCountTask] = useState(0);
 
   const handleAddToDo = () => {
-    let newToDoItem = {
-      task: newTask,
-    };
-    let updatedToDoArr = [...allTodos];
-    updatedToDoArr.push(newToDoItem);
-    setAllTodos(updatedToDoArr);
-    setNewTask('');
+    if (newTask !== '') {
+      let newToDoItem = {
+        task: newTask,
+      };
+      setCountTask(countTask + 1);
+
+      let updatedToDoArr = [...allTodos];
+      updatedToDoArr.push(newToDoItem);
+      setAllTodos(updatedToDoArr);
+      setNewTask('');
+    } else {
+      alert('Insert a task! (ㆆ_ㆆ)');
+    }
   };
 
   const handleDeleteToDo = (index) => {
     const updatedTodos = [...allTodos];
     updatedTodos.splice(index, 1);
     setAllTodos(updatedTodos);
+    setCountTask(countTask - 1);
+  };
+
+  const DeleteAlltasks = (index) => {
+    if (countTask === 0) {
+      alert("There's no tasks! ✅");
+    } else {
+      setAllTodos([]);
+      setCountTask(0);
+    }
   };
 
   return (
@@ -27,26 +44,47 @@ function App() {
       <form>
         <div className="border-left">
           <label htmlFor="exampleInputPassword1">
-            <h1>Mini Task</h1>
+            <h1>Mini Task Dashboard</h1>
           </label>
         </div>
-        <div>
-          <input
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-            type="text"
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Enter a task"
-          />
+        <div class="container">
+          <div class="row">
+            <div>
+              <input
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+                type="text"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Enter a task"
+              />
+            </div>
+            <div class="col">
+              <button
+                type="button"
+                className="btn btn-primary mt-2"
+                onClick={handleAddToDo}
+              >
+                add task
+              </button>
+            </div>
+            <div
+              class="col badge bg-primary text-wrap mt-1"
+              style={{ padding: '1rem' }}
+            >
+              <label class="fw-bold">{countTask} pending tasks</label>
+            </div>
+            <div class="col">
+              <button
+                type="button"
+                className="btn btn-primary mt-2"
+                onClick={DeleteAlltasks}
+              >
+                Delete all tasks
+              </button>
+            </div>
+          </div>
         </div>
-        <button
-          type="button"
-          className="btn btn-primary mt-2"
-          onClick={handleAddToDo}
-        >
-          add task
-        </button>
       </form>
 
       <div className="todo-list mt-2">
